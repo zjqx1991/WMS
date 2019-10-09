82189818bde2b8b2ee99f6f9419280e9ffa7e380
紫荆秋雪 <zjqx1991@163.com>
2019-10-09 17:39:16 +0800

    自定义登录拦截器

---
+++ src/main/java/com/revanwang/wms/web/interceptor/CheckLoginInterceptor.java
@@ -0,0 +1,21 @@
+package com.revanwang.wms.web.interceptor;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+import com.revanwang.wms.domain.Employee;
+
+/**
+ * 登录拦截器
+ */
+public class CheckLoginInterceptor extends AbstractInterceptor {
+    @Override
+    public String intercept(ActionInvocation actionInvocation) throws Exception {
+        Employee emp = (Employee) actionInvocation.getInvocationContext().getSession().get("user_in_session");
+        System.out.println("CheckLoginInterceptor.intercept:==" + emp);
+        if (null == emp) {
+            return Action.LOGIN;
+        }
+        return actionInvocation.invoke();
+    }
+}
